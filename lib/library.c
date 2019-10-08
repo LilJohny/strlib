a9c5b58a29e753242b001f58cfa824b2f4ee8c6f
denis <ivanenko@ucu.edu.ua>
2019-10-08 20:02:11 +0300

    Added creator

--- lib/library.c
+++ lib/library.c
@@ -1,13 +1,17 @@
 #include "library.h"
 #include <stdio.h>
+#include <stdlib.h>
 
+struct string{
+    size_t capacity_m;
+    size_t size_m;
+    char* data;
+};
 
-typedef struct
-{
-    size_t capacity_m; // Розмір блока
-    size_t size_m;	   // Фактичний розмір стрічки
-    char*  data;	   // Вказівник на блок пам'яті
-} string;
+int my_str_create(string* str, size_t buf_size){
+    str->data = (char *) malloc(15);
+    return 0;
+}
 
 void hello(void) {
     printf("Hello, World!\n");
