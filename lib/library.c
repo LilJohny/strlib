842561e792770281be41c7774530a8530355a10c
denis <ivanenko@ucu.edu.ua>
2019-10-08 19:26:06 +0300

    Initial commit

--- lib/library.c
+++ lib/library.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 
 
-struct  String{
-
-};
-
+typedef struct
+{
+    size_t capacity_m; // Розмір блока
+    size_t size_m;	   // Фактичний розмір стрічки
+    char*  data;	   // Вказівник на блок пам'яті
+} string;
 
 void hello(void) {
     printf("Hello, World!\n");
